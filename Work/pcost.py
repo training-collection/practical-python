9eff7a6ab4bba45ebc60118924fca81a283fb417
Irene Wallis <irene@cubicearth.nz>
2020-12-20 18:20:48 +1300

    Complete Ex 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,5 +2,12 @@
 #
 # Exercise 1.27
 
-with open('Data/protfolio.csv', 'rt') as f:
+total_cost = 0.0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
     for line in f:
+        row = line.split(',')
+        total_cost = total_cost + float(row[1]) * float(row[2])
+
+print(total_cost)
