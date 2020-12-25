a9f3f8d660435a4aec7f973ef5da71e4f9694a21
Irene Wallis <irene@cubicearth.nz>
2020-12-25 21:02:36 +1300

    Ex 1.31 Error handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,12 +2,19 @@
 #
 # Exercise 1.27
 
-total_cost = 0.0
+def portfolio_cost(filename):
+    'Calculates total cost of share portfolio'
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            row = line.split(',')
+            try:
+                total_cost = total_cost + float(row[1]) * float(row[2])
+            except ValueError:
+                print("Could not parse", line)
+    return total_cost
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        total_cost = total_cost + float(row[1]) * float(row[2])
 
-print(total_cost)
+cost = portfolio_cost('Data/missing.csv')  
+print(cost)
