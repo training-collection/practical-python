90318fa01bdf473780fdb181fdf39993845ebe20
Irene Wallis <irene@cubicearth.nz>
2020-12-25 21:12:06 +1300

    Using csv library (ex 1.32) '

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,17 +2,23 @@
 #
 # Exercise 1.27
 
+import csv
+
 def portfolio_cost(filename):
-    'Calculates total cost of share portfolio'
+    '''
+    Calculates total cost of share portfolio
+    Adusted to use csv package (ex 1.32)
+    '''
     total_cost = 0.0
     with open(filename, 'rt') as f:
-        headers = next(f)
-        for line in f:
-            row = line.split(',')
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            #row = line.split(',')
             try:
                 total_cost = total_cost + float(row[1]) * float(row[2])
             except ValueError:
-                print("Could not parse", line)
+                print("Could not parse", row)
     return total_cost
 
 
