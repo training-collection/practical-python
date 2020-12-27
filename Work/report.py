1bf344f2110efe0a93c6e441eacc2db842e8715d
Irene Wallis <irene@cubicearth.nz>
2020-12-27 18:58:04 +1300

    Ex 2.6 working

--- Work/report.py
+++ Work/report.py
@@ -4,8 +4,9 @@
 
 import csv
 
+'''
+# reads the file in as a tuple
 def read_portfolio(filename):
-    '''Reads portfolio as tuple'''
     portfolio = []
 
     with open(filename, 'rt') as f:
@@ -15,3 +16,36 @@ def read_portfolio(filename):
             holiding = (row[0], int(row[1]), float(row[2]))
             portfolio.append(holiding)
     return portfolio
+'''
+
+# reads the file in as a dictionary
+def read_portfolio(filename):
+    '''reads file as a dictionary'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        dict = {}
+        for row in rows:
+            dict = {'name': row[0], 'shares': int(row[1]), 'price': float(row[2])}
+            portfolio.append(dict)
+    return portfolio
+
+# read in csv (string, float) as a dictionary
+# handle blank lines
+
+def read_prices(filename):
+    '''reads file in as dictionary and handles NAN'''
+    
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        # note that there is no header
+
+        dict = {}
+        for row in rows:
+            try:
+                dict[row[0]] =  float(row[1])
+            except IndexError:
+                print("Could not parse", row)
+    return dict
