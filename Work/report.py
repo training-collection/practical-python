1c00d1b8b8cc66780a2105d93472502ae9f1f3e0
Irene Wallis <irene@cubicearth.nz>
2020-12-27 15:06:57 +1300

    Read files in as tuple ex 2.4)

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,17 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    '''Reads portfolio as tuple'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holiding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holiding)
+    return portfolio
