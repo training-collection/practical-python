f474d39e792e709a47d23729059120bc1bf2df83
Irene Wallis <irene@cubicearth.nz>
2021-01-06 11:57:40 +1300

    Ex 5.7 and 5.8 properties, setters and slots

--- Work/stock.py
+++ Work/stock.py
@@ -3,6 +3,8 @@
 # Ex 4.1
 
 class Stock:
+    __slots__ = ('name','_shares','price')
+    # slots makes code more memory efficent but should be avoided
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -16,7 +18,17 @@ class Stock:
     def __repr__(self):
     return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
     '''
+    @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected integer')
+        self._shares = value
 
+    @property
     def cost(self):
         ''' calculate shares*price '''
         return self.shares * self.price
